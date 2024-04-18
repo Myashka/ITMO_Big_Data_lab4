4aebb70c1df1bcb034a20fb18dc85f60eb30bcce
Alexey <porsh-5@yandex.ru>
2024-04-18 15:03:07 +0300

    Fix tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,34 +1,49 @@
 import pytest
-from httpx import AsyncClient
-
+# from httpx import AsyncClient
+from fastapi.testclient import TestClient
 from main import app
 
 
-@pytest.fixture
+@pytest.fixture()
 def client():
-    return AsyncClient(app=app, base_url="http://test")
+    client = TestClient(app)
+    yield client
 
 
-@pytest.mark.asyncio
+# @pytest.mark.asyncio
 async def test_index(client):
     response = await client.get("/")
     assert response.status_code == 200
     assert response.json() == {"index": "classification app working"}
 
 
-@pytest.mark.asyncio
+# @pytest.mark.asyncio
 async def test_read_results(client):
     response = await client.get("/results")
     assert response.status_code == 200
     assert response.json() == []
 
 
-@pytest.mark.asyncio
+# @pytest.mark.asyncio
 async def test_classify_input(client):
     client.__enter__()
 
-    message = "Love this beautiful country"
-    response = await client.post(f"/classify/{message}")
+    test_message = "Love this beautiful country"
+    response = await client.post(f"/classify/{test_message}")
+
+    result_data = response.json()
+    assert 'sentiment' in result_data
+    sentiment = result_data['sentiment']
+    assert type(sentiment) in (str, int)  # В зависимости от ожидаемого типа данных
+
+    # Проверяем, что результат добавлен в базу данных
+    response = client.get("/results")
+    assert response.status_code == 200
+    results = response.json()
+    assert len(results) == 1
+    assert results[0]['message'] == test_message
+    assert results[0]['sentiment'] == sentiment
+
     assert response.status_code == 200
     assert response.json() == {"sentiment": "positive"}
 
