c22c470d303c81277774f954d2f90e2bc98ab943
Alexey <porsh-5@yandex.ru>
2024-04-18 15:13:47 +0300

    Sync db sessions

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
 from requests import Request
-from sqlalchemy.ext.asyncio import AsyncSession
+# from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
 from sqlalchemy.future import select
 
 from src.classifier import Classifier
@@ -32,8 +33,8 @@ async def lifespan(app: FastAPI):
     app.state.preprocessor = Preprocessor(settings=app.state.preprocessor_settings)
     app.state.classifier = Classifier.load(config.load_path)
 
-    async with engine.begin() as conn:
-        await conn.run_sync(models.Base.metadata.create_all)
+    with engine.begin() as conn:
+        conn.run_sync(models.Base.metadata.create_all)
 
     yield
 
@@ -47,22 +48,22 @@ def index():
 
 
 @app.get("/results", status_code=200, response_model=List[DBResult])
-async def read_results(db: AsyncSession = Depends(get_db)):
-    async with db as session:
+def read_results(db: Session = Depends(get_db)):
+    with db as session:
         query = select(Result)
-        result = await session.execute(query)
+        result = session.execute(query)
         results = result.scalars().all()
         return results
 
 
 @app.post("/classify/{message}", status_code=200, response_model=PredictOutput)
-async def classify_input(message: str, db: AsyncSession = Depends(get_db)):
+def classify_input(message: str, db: Session = Depends(get_db)):
     try:
         processed_test: str = app.state.preprocessor(message)
         pred: PredictOutput = app.state.classifier.predict(processed_test)
         result = models.Result(message=message, sentiment=pred.sentiment)
         db.add(result)
-        await db.commit()
+        db.commit()
         return pred
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- src/db/database.py
+++ src/db/database.py
@@ -1,7 +1,8 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,8 +10,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 async def get_db():
