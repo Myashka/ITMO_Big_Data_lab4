7abfe9fb47dbc8dfc08051abbd942320f8c574cc
Alexey <porsh-5@yandex.ru>
2024-04-19 14:33:22 +0300

    Local done

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,26 +4,29 @@ from fastapi.testclient import TestClient
 from main import app
 
 
-@pytest.fixture
-def client():
-    client = TestClient(app)
-    yield client
+@pytest.fixture(scope="module")
+def test_app():
+    # Вызываем событие старта перед тестами
+    with TestClient(app) as client:
+        app.router.startup()
+        yield client  # Здесь тесты выполняются
+        app.router.shutdown()
             
-def test_read_main(client):
-    response =  client.get("/")
+def test_read_main(test_app):
+    response =  test_app.get("/")
     assert response.status_code == 200
     assert response.json() == {"index": "classification app working"}
 
-def test_requests(client):
+def test_requests(test_app):
     message = "Love this beautiful country"
-    response =  client.post(f"/classify/{message}")
+    response =  test_app.post(f"/classify/{message}")
     assert response.status_code == 200
     result_data =  response.json()
 
     assert 'sentiment' in result_data   
     sentiment = result_data['sentiment']
 
-    response =  client.get("/results")
+    response =  test_app.get("/results")
     assert response.status_code == 200
     results = response.json()
     assert len(results) == 1
